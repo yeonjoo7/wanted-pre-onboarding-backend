ed60e956a4c6317f588e1736f183b5c19e10d48f
yeonjoo7 <jenny.likes.breeze@gmail.com>
2023-08-11 19:40:10 +0900

    [FIX] User - 탈퇴한 회원은 로그인 못하게 수정

--- services/userService.js
+++ services/userService.js
@@ -2,7 +2,7 @@
 
 const jwt = require("jsonwebtoken");
 const Model = require('../common/model/modelManager');
-const { InvalidPasswordError, UserEmailAlreadyRegisteredError } = require('../common/modules/customErrors');
+const { InvalidPasswordError, UserEmailAlreadyRegisteredError, UserNotFoundError} = require('../common/modules/customErrors');
 
 function userEmailAndPasswordValidationCheck(email, password) {
     if (password.length < 8) {
@@ -26,8 +26,8 @@ exports.signIn = async (email, password) => {
     try {
         userEmailAndPasswordValidationCheck(email, password);
         const user = await Model.User.findOne({ where:{ email: email }});
+        if(!user || user.status !== 'ALIVE') throw new UserNotFoundError;
         const token = jwt.sign({data: user.id}, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
-
         return { userId: user.id, token: token };
     } catch (e) {
         throw e;
